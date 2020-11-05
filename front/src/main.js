1dd69ee3175bc48fc139255bd5ef84dd8dd08d30
mateusedival <mateus.edival@gmail.com>
2020-11-05 20:57:35 -0300

    toggle

--- front/src/main.js
+++ front/src/main.js
@@ -1,13 +1,30 @@
 btnMap = document.querySelector("#btnMap");
 btnForms = document.querySelector('#btnForms');
 maps = document.querySelector('#map')
+forms = document.querySelector('#forms')
+
+forms.style.display='none'
+setDisplay(forms.children,'none')
 
 btnMap.onclick = function() {
-    let display =  maps.style.display
-    if(display == 'none') 
+    forms.style.display = 'none'
+    setDisplay(forms.children,'none')
+    if(maps.style.display == 'none') 
         maps.style.display = 'block';
 }
 
 btnForms.onclick = function() {
     maps.style.display='none';
+    if(forms.style.display == 'none') 
+    {
+        forms.style.display = 'block';
+        setDisplay(forms.children,'block')
+    }
+}
+
+function setDisplay(array,state) {
+    for (i = 0; i < array.length; i++)
+    {
+        array[i].style.display = state;
+    }
 }
